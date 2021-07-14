992e8739cafb7f5df81d3f1a9f85daee476db55f
Dilpreet <67494637+Dilpreet05@users.noreply.github.com>
2021-07-14 17:04:48 -0400

    First Commit

---
+++ JAVA/findOdd.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class findOdd {
+        public static void main(String[] args) {
+                
+                Scanner scan = new Scanner(System.in);
+
+                for (int i = 0; i <= 100; i++) {
+                        if (i%2 != 0){
+                                System.out.println(i);
+                        }
+                }
+
+
+
+        }
+}
