2d1e0d83f86cecddaf6d8b029234540030bc92bf
Dilpreet <67494637+Dilpreet05@users.noreply.github.com>
2021-07-25 10:41:19 -0400

    commit

---
+++ password_gen.py
@@ -0,0 +1,12 @@
+import random
+
+chars = "abcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*"
+while 1:
+        pass_len = int(input("How long would you like the password : "))
+        pass_amount = int(input("how many passwords would you like to create : "))
+        for i in range(0, pass_amount):
+                password = ""
+                for x in range(0, pass_len):
+                        pass_char = random.choice(chars)
+                        password+=pass_char
+                print(password)
