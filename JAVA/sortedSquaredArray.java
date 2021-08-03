b6a62070dca7ffa8eec8273368d761dace93d5ee
Dilpreet <67494637+Dilpreet05@users.noreply.github.com>
2021-08-03 13:32:56 -0400

    practice

---
+++ JAVA/sortedSquaredArray.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+
+public class sortedSquaredArray {
+        public static void main(String[] args) {
+                
+                int[] array = {-6,-4,1,2,3,5};
+                for(int i = 0; i < array.length; i++){
+                        if(array[i] < 0){
+                                array[i]*=-1;
+                        }
+                }
+                Arrays.sort(array);
+                for(int i = 0; i < array.length; i++){
+                        array[i]*=array[i];
+                }
+
+                System.out.println(Arrays.toString(array));
+                
+        }
+}
